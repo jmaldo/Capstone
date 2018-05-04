90beae48141e7b2444745b14c582658b7d12b23d
Jeremy Maldonado <jeremymaldonado99@hotmail.com>
2018-05-03 21:19:20 -0500

    Database file written.

---
+++ EventPromo/Data/EventDBContext.cs
@@ -0,0 +1,24 @@
+﻿//using EventMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPromo.Data
+{
+    public class EventDBContext : DbContext
+    {
+        public DbSet<Event> Event { get; set; }
+        public DbSet<EventCategory> Categories { get; set; }
+
+        public DbSet<Lookup> Lookup { get; set; }
+        public DbSet<Lookup> Lookup { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Lookup>()
+                .HasKey(c => { c.CheeseID, c.MenuID });
+        }
+
+        public EventDBContext(DbContextOptions<EventDbContext> options)
+            : base(options)
+        { }
+    }
+}
